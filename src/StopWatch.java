828a00a8e9eec4d45f265041a82d30cd0cf4d9d2
omipapal12 <omipapal12@gmail.com>
2021-07-17 14:23:32 +0530

    Added

---
+++ src/StopWatch.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.Scanner;
+
+import jdk.internal.icu.impl.Utility;
+
+
+public class StopWatch {
+	public static void main(String[] args) {
+		System.out.println("Press any character and press Enter to SATRT the Stopwatch");
+		char startCh =  Utility.getUserChar();
+		long start = System.currentTimeMillis();
+		System.out.println("Enter any character and press Enter to STOP the Stopwatch");
+		char stopCh = Utility.getUserChar();
+		long stop = System.currentTimeMillis();
+		float timeElapsed =(float) (stop - start) / 1000;
+		System.out.println("Time = " + timeElapsed + " Seconds");
+		}
+}
